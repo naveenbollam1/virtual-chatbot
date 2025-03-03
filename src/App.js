bf33ba70f9961d4fc642bac5dcacceda08121ec1
naveenbollam1 <naveenbollam001@gmail.com>
2025-03-03 08:38:14 +0530

    fix merge conflicts

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>My Journey with React</h2>
+        <p>
+          Hello, I'm learning React!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
